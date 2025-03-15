85c7c8f26cff3b138d384400b9f38fa1c63f2008
Hemanthmeesala <hemanth.meesala2004@gmail.com>
2025-03-15 13:28:46 +0530

    Functions Completed

---
+++ Functions.java
@@ -0,0 +1,28 @@
+public class Functions {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+
+        // arthimatic operators 
+
+        // + - * / % 
+
+        // Relational operators
+
+        // < > <= >= == !=
+
+        // Logical operators
+
+        // && || !
+
+    //     int a = 10;
+
+    //     if(a > 9)
+    //     System.out.print("HI");
+
+    //     else 
+    //     System.out.print("BYE");
+
+
+    
+    }
+}
