7aeb0f55f53771da1e7daf7656ddd27d0e2bff19
Hemanthmeesala <hemanth.meesala2004@gmail.com>
2025-03-15 13:16:42 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,30 @@
+class Hello {
+    public static void main (String a[]){
+        System.out.print("Hello World");
+
+        // Variiables
+        // int num1 = 3;
+        // int num2 = 4;
+        // System.out.println(num1 + num2);
+
+        // Data Types
+        // int num1 = 3;
+        // short num1 = 3;
+        // long num4 = 17329;
+        // float num2 = 4.5f;
+        // double num2 = 4.5;
+        // char letter = 'a';
+        // boolean isTrue = true;
+
+        // Literals
+
+        // int num = 1_00_00_000;
+        // double num2 = 1_00_00_000.00;
+        // char c = 'a';
+        // c++;
+        // System.out.println(c); b 
+
+        
+
+    }
+}
