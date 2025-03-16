603d7b3d96e3dcc799690ea4645487aa2cc74470
Hemanthmeesala <hemanth.meesala2004@gmail.com>
2025-03-16 10:56:42 +0530

    Strings

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static void main(String a[]){
+        // String name = new String("Hemanth");
+        String name = "Hemanth";
+        System.out.println(name);
+
+        // Mutable - Which can be Chnaged
+        // Immutable - which cannot be changed
+
+        StringBuffer sb = new StringBuffer("Hemanth");
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
+    }
+}
